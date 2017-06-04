977dc3093437bdf9af06b36b36d81d429d46f0db
Andy Fragen <andy@thefragens.com>
2017-06-04 12:18:53 -0700

    directly cleanup after myself

--- uninstall.php
+++ uninstall.php
@@ -29,6 +29,10 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-include_once( 'translations-updater.php' );
-$base = new Fragen\Translations_Updater\Base();
-$base->delete_all_cached_data();
+global $wpdb;
+
+$table         = is_multisite() ? $wpdb->base_prefix . 'sitemeta' : $wpdb->base_prefix . 'options';
+$column        = is_multisite() ? 'meta_key' : 'option_name';
+$delete_string = 'DELETE FROM ' . $table . ' WHERE ' . $column . ' LIKE %s LIMIT 1000';
+
+$wpdb->query( $wpdb->prepare( $delete_string, array( '%ghu-%' ) ) );
