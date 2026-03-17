e06461891656801b2c7c66fddca58204f46789e0
Andy Fragen <andy@thefragens.com>
2026-03-17 09:33:21 -0700

    simplify as variable already declared

--- src/Translations_Updater/Language_Pack_API.php
+++ src/Translations_Updater/Language_Pack_API.php
@@ -132,23 +132,23 @@ class Language_Pack_API {
 		switch ( $git ) {
 			// phpcs:disable WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_decode
 			case 'github':
-				$response = $this->api( "/repos/{$headers['owner']}/{$headers['repo']}/contents/language-pack.json" );
+				$response = $this->api( "/repos/{$headers['owner_repo']}/contents/language-pack.json" );
 				$response = isset( $response->content )
 					? json_decode( base64_decode( $response->content ) )
 					: null;
 				break;
 			case 'bitbucket':
-				$response = $this->api( "/2.0/repositories/{$headers['owner']}/{$headers['repo']}/src/{$type['branch']}/language-pack.json" );
+				$response = $this->api( "/2.0/repositories/{$headers['owner_repo']}/src/{$type['branch']}/language-pack.json" );
 				break;
 			case 'gitlab':
-				$id       = rawurlencode( $headers['owner'] . '/' . $headers['repo'] );
+				$id       = rawurlencode( $headers['owner_repo'] );
 				$response = $this->api( "/projects/{$id}/repository/files/language-pack.json" );
 				$response = isset( $response->content )
 					? json_decode( base64_decode( $response->content ) )
 					: null;
 				break;
 			case 'gitea':
-				$response = $this->api( "/repos/{$headers['owner']}/{$headers['repo']}/raw/{$type['branch']}/language-pack.json" );
+				$response = $this->api( "/repos/{$headers['owner_repo']}/raw/{$type['branch']}/language-pack.json" );
 				$response = isset( $response->content )
 					? json_decode( base64_decode( $response->content ) )
 					: null;
